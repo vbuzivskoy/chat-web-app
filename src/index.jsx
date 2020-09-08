c8f617e1bfab561698412d315c1a3a0132a7de79
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-09-08 15:04:44 +0300

    Rename frontend index.js file to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,14 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+
 import '../assets/application.scss';
 
 // import faker from 'faker';
+// eslint-disable-next-line
 import gon from 'gon';
 // import cookies from 'js-cookie';
 // import io from 'socket.io-client';
@@ -13,6 +18,19 @@ import gon from 'gon';
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
+
+const renderApp = ({ channels, messages, currentChannelId }) => (
+  <App
+    channels={channels}
+    messages={messages}
+    currentChannelId={currentChannelId}
+  />
+);
+
+ReactDOM.render(
+  renderApp(gon),
+  document.getElementById('chat'),
+);
 
 console.log('it works!');
 console.log('gon', gon);
