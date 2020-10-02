9557457a34b414318a7ae194a805e5df638ded8d
Buzivskoy Vyacheslav <vyacheslav.buzivskoy@ukmig.com.ua>
2020-10-02 13:02:01 +0300

    Update frontend entry point 'index.jsx' to support sockets, state and react components

--- src/index.jsx
+++ src/index.jsx
@@ -3,32 +3,43 @@ import 'regenerator-runtime/runtime';
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-import App from './App';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 
 import '../assets/application.scss';
 
-// import faker from 'faker';
-// eslint-disable-next-line
+import faker from 'faker';
 import gon from 'gon';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
+import Cookies from 'js-cookie';
+import io from 'socket.io-client';
+
+import rootReducer from './reducers';
+import App from './components/App';
+import UsernameContext from './username-context';
+import listenSockets from './sockets';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const renderApp = ({ channels, messages, currentChannelId }) => (
-  <App
-    channels={channels}
-    messages={messages}
-    currentChannelId={currentChannelId}
-  />
-);
+const currentUsernameCooky = Cookies.get('username');
+const currentUsername = currentUsernameCooky
+  || faker.fake('{{name.lastName}}_{{name.firstName}}{{random.number}}');
+Cookies.set('username', currentUsername);
+
+const store = configureStore({
+  reducer: rootReducer,
+  preloadedState: gon,
+});
 
 ReactDOM.render(
-  renderApp(gon),
+  <UsernameContext.Provider value={currentUsername}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </UsernameContext.Provider>,
   document.getElementById('chat'),
 );
 
-console.log('it works!');
-console.log('gon', gon);
+const socket = io();
+listenSockets(socket, store);
